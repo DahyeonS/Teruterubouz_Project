cfcdfbd1597b19a702772b76f3a5949c01c4066a
Dahyeon Seo <siren199704@gmail.com>
2023-10-23 18:20:40 +0900

    Update temperature.py

--- python/temperature.py
+++ python/temperature.py
@@ -5,3 +5,13 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv("D:/kdigital2307/github/Teruterubouz_Project/python/기상청데이터_기온.csv").iloc[:, [2, 3, 4, 5, 7, 8]]
+
+def year() :
+    avg_temp = data.groupby(data['일시'].str[:4]).mean()['평균기온(℃)']
+    max_temp = data.groupby(data['일시'].str[:4]).mean()['평균최고기온(℃)']
+    avg_temp = data.groupby(data['일시'].str[:4]).mean()['평균최저기온(℃)']
+    
+    result = [avg_temp, max_temp, avg_temp]
+    return result
+
+print(year())
