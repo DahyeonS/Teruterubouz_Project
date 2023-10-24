d1db3f2697d42d7dfd805572a40ba1c90bf1e0ce
Dahyeon Seo <siren199704@gmail.com>
2023-10-24 10:08:35 +0900

    Update temperature.py

--- python/temperature.py
+++ python/temperature.py
@@ -2,16 +2,21 @@
 # 기온 데이터 처리
 
 import pandas as pd
-import numpy as np
 
 data = pd.read_csv("D:/kdigital2307/github/Teruterubouz_Project/python/기상청데이터_기온.csv").iloc[:, [2, 3, 4, 5, 7, 8]]
 
-def year() :
-    avg_temp = data.groupby(data['일시'].str[:4]).mean()['평균기온(℃)']
-    max_temp = data.groupby(data['일시'].str[:4]).mean()['평균최고기온(℃)']
-    avg_temp = data.groupby(data['일시'].str[:4]).mean()['평균최저기온(℃)']
+def year():
+    avg_temp = round(data.groupby(data['일시'].str[:4]).mean()['평균기온(℃)'], 1)[:-1]
+    max_temp = data.groupby(data['일시'].str[:4]).max()['평균최고기온(℃)'][:-1]
+    min_temp = data.groupby(data['일시'].str[:4]).min()['평균최저기온(℃)'][:-1]
     
-    result = [avg_temp, max_temp, avg_temp]
+    result = pd.DataFrame({'평균기온':avg_temp, '평균최고기온':max_temp, '평균최저기온':min_temp})
     return result
 
-print(year())
+def month(): 
+    avg_temp = round(data.groupby(data['일시'].str[5:7]).mean()['평균기온(℃)'], 1)
+    max_temp = data.groupby(data['일시'].str[5:7]).max()['평균최고기온(℃)']
+    min_temp = data.groupby(data['일시'].str[5:7]).min()['평균최저기온(℃)']
+    
+    result = pd.DataFrame({'평균기온':avg_temp, '평균최고기온':max_temp, '평균최저기온':min_temp})
+    return result
