1879f28638aee504bf9446d5fd8fcff50eef9e24
Dahyeon Seo <siren199704@gmail.com>
2023-10-24 17:00:44 +0900

    코드 수정

--- python/app.py
+++ python/app.py
@@ -6,9 +6,8 @@ from flask import Flask
 from flask_cors import CORS
 import weather_info
 import temperature
-import temp_dt
 import temp_rf
-import temp_xgb
+import temp_svm
 import precipitation
 
 # 2. 결과물을 웹에 전송
@@ -31,68 +30,28 @@ def incheon():
 def busan():
     return weather_info.weather('11H20201')
 
-@app.route('/temp/year') # 연별 기온 통계
-def temp_year_info():
-    return temperature.temp_year_info()
+@app.route('/temp') # 기온 통계
+def temp_info():
+    return temperature.temp_info()
 
-@app.route('/temp/month') # 월별 기온 통계
-def temp_month_info():
-    return temperature.temp_month_info()
+@app.route('/temp/rf') # 월별 기온 예측 - 랜덤포레스트
+def temper_rf():
+    return temp_rf.result()
 
-@app.route('/temp/year/dt') # 연별 기온 예측 - 의사결정나무
-def temp_year_dt():
-    return temp_dt.year()
+@app.route('/temp/svm') # 월별 기온 예측 - 서포트 벡터머
+def temper_svm():
+    return temp_svm.result()
 
-@app.route('/temp/month/dt') # 월별 기온 예측 - 의사결정나무
-def temp_month_dt():
-    return temp_dt.month()
+@app.route('/rain') # 월별 강수량 통계
+def rain_info():
+    return precipitation.rain_info()
 
-@app.route('/temp/year/rf') # 연별 기온 예측 - 랜덤포레스트
-def temp_year_rf():
-    return temp_rf.year()
+@app.route('/rain/rf') # 월별 강수량 예측 - 랜덤포레스트
+def rain_rf():
+    return rain_rf.result()
 
-@app.route('/temp/month/rf') # 월별 기온 예측 - 랜덤포레스트
-def temp_month_rf():
-    return temp_rf.month()
-
-@app.route('/temp/year/xgb') # 연별 기온 예측 - 부스팅
-def temp_year_xgb():
-    return temp_xgb.year()
-
-@app.route('/temp/month/xgb') # 월별 기온 예측 - 부스팅
-def temp_month_xgb():
-    return temp_xgb.month()
-
-@app.route('/rain/year') # 연별 강수량 통계
-def rain_year_info():
-    return precipitation.rain_year_info()
-
-@app.route('/rain/month') # 월별 강수량 통계
-def rain_month_info():
-    return precipitation.rain_month_info()
-
-@app.route('/rain/year/dt') # 연별 강수량 예측 - 의사결정나무
-def rain_year_dt():
-    return rain_dt.year()
-
-@app.route('/rain/month/dt') # 월별 강수량 예측 - 의사결정나무
-def rain_month_dt():
-    return rain_dt.month()
-
-@app.route('/rain/year/rf') # 연별 강수량 예측 - 랜덤포레스트
-def rain_year_rf():
-    return rain_rf.year()
-
-@app.route('/rain/month/rf') # 월별 강수량 예측 - 랜덤포레스트
-def rain_month_rf():
-    return rain_rf.month()
-
-@app.route('/rain/year/xgb') # 연별 강수량 예측 - 부스팅
-def rain_year_xgb():
-    return rain_xgb.year()
-
-@app.route('/rain/month/xgb') # 월별 강수량 예측 - 부스팅
-def rain_month_xgb():
-    return rain_xgb.month()
+@app.route('/rain/svm') # 월별 기온 예측 - 서포트 벡터머
+def rain_svm():
+    return rain_svm.result()
 
 app.run(host='127.0.0.1',debug=True, port=5050)
