f5a9115518a9631761ad690b1d35eeb5101858b2
Dahyeon Seo <siren199704@gmail.com>
2023-10-24 13:08:15 +0900

    Update temp_xgb.py

--- python/temp_xgb.py
+++ python/temp_xgb.py
@@ -8,7 +8,7 @@ import temperature
 # (2) 데이터셋 분리
 from sklearn.model_selection import train_test_split
 # (3) 모델링
-from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 # (4) 모델 평가
 from sklearn.metrics import mean_squared_error
 # (5) 결과값 전송
@@ -27,46 +27,47 @@ y = np.array(year['평균기온(℃)']).reshape(-1, 1)
 X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size = 0.25, random_state = 123)
 
 # 모델링
-rf = RandomForestRegressor()
-rf.fit(X_tr, y_tr)
-pred = rf.predict(X_val)
+xgb = XGBRegressor()
+xgb.fit(X_tr, y_tr)
+pred = xgb.predict(X_val)
+print("XGBoost -", mean_squared_error(y_val, pred))
 
 # 모델 적용
-model = RandomForestRegressor()
+model = XGBRegressor()
 model.fit(X, y)
-prediction = model.predict([['2024']])
+prediction = model.predict([[2024]])
 year_avg = round(prediction[0], 1)
 
 # (2). 최고기온 예측
-X = np.array(year['일시']).reshape(-1, 1)
 y = np.array(year['최고기온(℃)']).reshape(-1, 1)
 
 # 데이터셋 분리
 X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size = 0.25, random_state = 123)
 
 # 모델링
-rf.fit(X_tr, y_tr)
-pred = rf.predict(X_val)
+xgb.fit(X_tr, y_tr)
+pred = xgb.predict(X_val)
+print("XGBoost -", mean_squared_error(y_val, pred))
 
 # 모델 적용
 model.fit(X, y)
-prediction = model.predict([['2024']])
+prediction = model.predict([[2024]])
 year_max = round(prediction[0], 1)
 
 # (3). 최저기온 예측
-X = np.array(year['일시']).reshape(-1, 1)
 y = np.array(year['최저기온(℃)']).reshape(-1, 1)
 
 # 데이터셋 분리
 X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size = 0.25, random_state = 123)
 
 # 모델링
-rf.fit(X_tr, y_tr)
-pred = rf.predict(X_val)
+xgb.fit(X_tr, y_tr)
+pred = xgb.predict(X_val)
+print("XGBoost -", mean_squared_error(y_val, pred))
 
 # 모델 적용
 model.fit(X, y)
-prediction = model.predict([['2024']])
+prediction = model.predict([[2024]])
 year_min = round(prediction[0], 1)
 
 # 4. 월간 데이터 분석
@@ -78,8 +79,9 @@ y = np.array(month['평균기온(℃)']).reshape(-1, 1)
 X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size = 0.25, random_state = 123)
 
 # 모델링
-rf.fit(X_tr, y_tr)
-pred = rf.predict(X_val)
+xgb.fit(X_tr, y_tr)
+pred = xgb.predict(X_val)
+print("XGBoost -", mean_squared_error(y_val, pred))
 
 # 모델 적용
 model.fit(X, y)
@@ -88,15 +90,15 @@ prediction = model.predict(month_list)
 month_avg = np.round(prediction, 1)
 
 # (2). 최고기온 예측
-X = np.array(month['일시']).reshape(-1, 1)
 y = np.array(month['최고기온(℃)']).reshape(-1, 1)
 
 # 데이터셋 분리
 X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size = 0.25, random_state = 123)
 
 # 모델링
-rf.fit(X_tr, y_tr)
-pred = rf.predict(X_val)
+xgb.fit(X_tr, y_tr)
+pred = xgb.predict(X_val)
+print("XGBoost -", mean_squared_error(y_val, pred))
 
 # 모델 적용
 model.fit(X, y)
@@ -105,15 +107,15 @@ prediction = model.predict(month_list)
 month_max = np.round(prediction, 1)
 
 # (3). 최저기온 예측
-X = np.array(month['일시']).reshape(-1, 1)
 y = np.array(month['최저기온(℃)']).reshape(-1, 1)
 
 # 데이터셋 분리
 X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size = 0.25, random_state = 123)
 
 # 모델링
-rf.fit(X_tr, y_tr)
-pred = rf.predict(X_val)
+xgb.fit(X_tr, y_tr)
+pred = xgb.predict(X_val)
+print("XGBoost -", mean_squared_error(y_val, pred))
 
 # 모델 적용
 model.fit(X, y)
@@ -127,9 +129,9 @@ def year():
     
     item = {}
     item['year'] = 2024
-    item['avg'] = year_avg
-    item['max'] = year_max
-    item['min'] = year_min
+    item['avg'] = year_avg.astype('float64')
+    item['max'] = year_max.astype('float64')
+    item['min'] = year_min.astype('float64')
     data.append(item)
     
     result = json.dumps(data)
@@ -141,9 +143,9 @@ def month():
     for i in range(12) :
         item = {}
         item['month'] = i+1
-        item['avg'] = month_avg[i]
-        item['max'] = month_max[i]
-        item['min'] = month_min[i]
+        item['avg'] = round(month_avg[i].astype('float64'), 1)
+        item['max'] = round(month_max[i].astype('float64'), 1)
+        item['min'] = round(month_min[i].astype('float64'), 1)
         data.append(item)
     
     result = json.dumps(data)
