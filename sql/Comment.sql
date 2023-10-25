828dcc1c3edda2cd74e1b9044c49ce9f4c53ca41
Dahyeon Seo <siren199704@gmail.com>
2023-10-25 15:45:05 +0900

    SQL 수정

--- sql/Comment.sql
+++ sql/Comment.sql
@@ -1,10 +1,10 @@
 -- 테이블 생성
 CREATE TABLE comment (
-    id VARCHAR(10) NOT NULL,
+    id VARCHAR(20) NOT NULL,
     num INT PRIMARY KEY NOT NULL,
     post_num INT NOT NULL,
-    nickname VARCHAR(20) NOT NULL,
-    context VARCHAR(4000) NOT NULL,
+    nickname VARCHAR(30) NOT NULL,
+    content VARCHAR(2000) NOT NULL,
     postdate DATETIME DEFAULT NOW() NOT NULL,
     FOREIGN KEY (id) REFERENCES member(id) ON DELETE CASCADE,
     FOREIGN KEY (post_num) REFERENCES post(num) ON DELETE CASCADE,
