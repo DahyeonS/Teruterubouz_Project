d3c0d0f45c102659b1ba789af0819a78e5c41d1b
Dahyeon Seo <siren199704@gmail.com>
2023-11-07 15:36:45 +0900

    테이블 추가

---
+++ sql/notice.sql
@@ -0,0 +1,13 @@
+-- 테이블 생성
+CREATE TABLE notice (
+    num INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    content VARCHAR(4000) NOT NULL,
+    province VARCHAR(20) NOT NULL,
+    postdate DATETIME DEFAULT NOW() NOT NULL,
+    update_date DATETIME,
+    visit_count INT DEFAULT 0
+);
+
+-- 확인
+SELECT * FROM notice;
